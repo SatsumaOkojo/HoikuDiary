fc2fd6f3f420b09bfb92337ffcd38389b2aef40e
SatsumaOkojo <onisimazu23@ezweb.ne.jp>
2022-12-10 22:31:41 +0900

    create api_3

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,15 +14,4 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-
-Route::get('users', 'UserController@getAllUsers');
-Route::get('users/{id}', 'UserController@getUser');
-Route::post('users', 'UserController@createUser');
-Route::put('users/{id}', 'UserController@updateUser');
-route::delete('users/{id}', 'UserController@deleteUser');
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
+Route::get('/user', [UserController::class, 'user']);
